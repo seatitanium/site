d6a350ccae1f282ca48ce290cb778a2bcf6e9804
Subilan <suyuanliang1995@gmail.com>
2021-08-23 19:12:15 +0800

    dep: add style resources loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require("path");
+
 module.exports = {
-    devServer: {
-        proxy: 'http://localhost:5000'
-    }
-}
+	devServer: {
+		proxy: 'http://localhost:5000'
+	},
+	pluginOptions: {
+		'style-resources-loader': {
+			preProcessor: 'less',
+			patterns: [path.resolve(__dirname, './src/styles/*.less')]
+		}
+	},
+
+	productionSourceMap: false
+};
