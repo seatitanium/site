dc96d9b08b2c31c336f2b37b7e28863bd9158490
Subilan <suyuanliang1995@gmail.com>
2021-08-26 10:10:35 +0800

    feat: join page

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -11,7 +11,7 @@
 			</template>
 		</banner>
 		<div class="container">
-			<div class="server-status">
+			<div class="server-status content">
 				<h1 class="primary-text">服务器概况</h1>
 				<meta-bar>
 					<meta-item icon="minecraft">
@@ -70,6 +70,89 @@
 					>
 				</div>
 			</div>
+			<div class="features content">
+				<div class="hero-box">
+					<h1 class="primary-text">三个概念</h1>
+					<p>有助于你更好地了解 SEATiDE 服务器的运行模式</p>
+				</div>
+				<div class="feature-box">
+					<div class="feature black">
+						<mdicon name="human-handsup" />
+						<span>DEMOCRACY</span>
+						<h1>民主</h1>
+						<p class="typo light">
+							服务器的模组选取<strong>不由管理员主导</strong>，而是由玩家共同决定。官方将预先选择一些模组主题，具体模组则由玩家自由决定。
+							<br />决定方式有很多种，一般我们采取<strong>语音聊天或者文字交流</strong>，人人平等，仅需给出你的合理理由即可参与！
+						</p>
+					</div>
+					<div class="feature primary">
+						<mdicon name="console" />
+						<span>TECHNOLOGY</span>
+						<h1>技术</h1>
+						<p class="typo light">
+							在
+							SEATiDE，我们<strong>提倡使用各种技术</strong>来解决一些原本看似比较棘手的问题，并尽可能发挥长处，减少痛苦。<br />诚然，一个人不可能是全能的，所以需要来自多方的协助。我们提倡互相学习，即使你什么都不了解，也不必担心。
+						</p>
+					</div>
+					<div class="feature white">
+						<mdicon name="cog" />
+						<span>AUTOMATION</span>
+						<h1 class="primary-text">自动化</h1>
+						<p class="typo light">
+							得益于阿里云的
+							API，我们可以做到自动备份，甚至编写插件，通过让服务器在无人在线超过
+							12
+							小时后自动关闭来节省经费，投入到更应该使用的地方。<br />在一些看不见的地方，我们也使用了自动化，这是为了节省和简化开服体验。
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="join content">
+				<h1 class="primary-text">立即加入</h1>
+				<p class="typo">
+					服务器的 IP
+					会不定期变动，故不公开，建议直接加入讨论群以保持消息的更新。
+				</p>
+				<p class="typo">
+					SEATiDE 当前主要以 QQ
+					群的形式存在，同时也有微信讨论群以便交流，没有运营团队。SEATiDE
+					<strong>没有</strong>加入门槛，仅需<strong>
+						Java 正版</strong
+					>即可进入服务器。同时，建议使用<strong>内存 4GB+</strong>
+					的电脑以防模组加载导致的游戏卡顿。
+				</p>
+				<div class="join-cards">
+					<a class="join-card qq" href="https://qm.qq.com/cgi-bin/qm/qr?k=tcoJ_CF6AjAWGQS62TxGRIidea_4tqw7&jump_from=webapi" target="_blank">
+						<mdicon name="arrow-right" />
+						<img src="@/assets/tencent-qq.svg" />
+						<div class="text">
+							<h1>QQ 讨论群</h1>
+							<h2>
+								点击加入，或搜索群号 820912578<br />验证信息请填写你的正版
+								ID
+							</h2>
+						</div>
+					</a>
+					<div class="join-card wechant">
+						<mdicon name="qrcode-scan" />
+						<img src="@/assets/tencent-wechant.svg" />
+						<div class="text">
+							<h1>微信讨论群</h1>
+							<h2>由于限制，加入 QQ 讨论群后扫码可加</h2>
+						</div>
+					</div>
+				</div>
+				<p class="typo">
+					加入以后，你可以在<strong>群公告</strong>中找到最新的服务器详细信息（包含<strong>版本</strong>、<strong>IP</strong>、<strong>客户端和模组包下载地址</strong>等）。同时，你也将会被<strong>正式认定</strong>为「SEATiDE」玩家。接下来，你只需要准备好游戏，然后加入服务器即可开始你的
+					SEATiDE 之旅！
+				</p>
+				<p class="typo">
+					如果你对模组有些生疏，不知道如何开始，或者拥有其它问题，你可以在<strong>群聊里询问</strong>，或者也可以阅读<strong
+						>我们编写的<a href="#">维基</a></strong
+					>。维基相当于是本电子书，上面归纳了大家觉得有用的要点，这本书由所有人一起编写，为了让你更快地解决问题
+					:)
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -114,7 +197,7 @@ export default Vue.extend({
 </script>
 
 <style lang="less" scoped>
-.server-status {
+.content {
 	padding: 32px 0;
 	.primary-text {
 		font-size: 49.5px;
@@ -123,8 +206,192 @@ export default Vue.extend({
 
 	p {
 		font-size: 22px;
+		margin: 8px 0;
 	}
+}
 
+.join {
+	.join-cards {
+		margin: 16px 0;
+		display: flex;
+		align-items: stretch;
+
+		.join-card {
+			width: 50%;
+			.mdi {
+				position: absolute;
+				right: 24px;
+				top: 16px;
+				opacity: 0.3;
+				transition: all 0.2s ease;
+			}
+			transition: all 0.2s ease;
+			&:hover {
+				.mdi {
+					opacity: 1;
+				}
+				opacity: 0.5;
+			}
+			position: relative;
+			border-radius: 5px;
+			box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+			padding: 16px 24px;
+			display: flex;
+			flex-wrap: nowrap;
+			align-items: center;
+			cursor: pointer;
+
+			img {
+				width: 100px;
+				height: 100px;
+				transform: scale(1.1);
+			}
+			.text {
+				padding-left: 16px;
+				h2 {
+					font-weight: normal;
+				}
+
+				h1,
+				h2 {
+					margin: 8px 0;
+				}
+
+				h2 {
+					font-size: 18px;
+				}
+			}
+			margin: 0 8px;
+
+			&.qq {
+				background: linear-gradient(-96deg, #1680c7, #12b7f5);
+				color: white;
+				h2 {
+					color: @textlightwhite;
+				}
+				&:hover .mdi {
+					transform: translateX(10px);
+				}
+			}
+
+			&.wechant {
+				h2 {
+					color: @textlightgray;
+				}
+			}
+		}
+	}
+}
+
+.hero-box {
+	p {
+		color: @textlightgray;
+		max-width: 600px;
+		display: block;
+		margin: auto;
+	}
+	margin-bottom: 64px;
+	text-align: center;
+}
+
+.features {
+	.feature-box {
+		display: flex;
+		align-items: stretch;
+	}
+}
+
+.features {
+	display: flex;
+	flex-direction: column;
+
+	.feature {
+		width: 33.333%;
+		margin: 0 8px;
+		text-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+		box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+
+		&.black {
+			background: #212121;
+		}
+
+		&.primary {
+			background: @primaryg;
+		}
+
+		&.white {
+			h1 {
+				text-shadow: none;
+			}
+			p {
+				color: @textlightgray;
+				strong {
+					color: @textgray;
+				}
+			}
+			.mdi {
+				svg {
+					fill: @textgray;
+				}
+			}
+			span {
+				color: @textgray;
+			}
+		}
+
+		&:first-child {
+			margin-left: 0;
+		}
+		&:last-child {
+			margin-right: 0;
+		}
+		border-radius: 12px;
+		transition: 0.2s ease;
+		color: white;
+		padding: 32px;
+
+		h1 {
+			margin-top: 0;
+			margin-bottom: 8px;
+			font-size: 40px;
+		}
+
+		p {
+			font-size: 18px;
+			z-index: 2;
+		}
+		overflow: hidden;
+		.mdi {
+			opacity: 0.4;
+			position: absolute;
+			right: 64px;
+			bottom: 64px;
+			transition: all 0.2s ease;
+			z-index: 1;
+			width: 24px;
+			height: 24px;
+			transform: scale(10);
+		}
+
+		position: relative;
+
+		&:hover {
+			.mdi {
+				opacity: 0.7;
+			}
+		}
+
+		> span:not(.mdi) {
+			font-family: Consolas, monospace;
+			position: absolute;
+			opacity: 0.3;
+			right: 32px;
+			top: 32px;
+		}
+	}
+}
+
+.server-status {
 	.mods {
 		width: 100%;
 		display: flex;
