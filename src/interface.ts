eccd5671dc9c2085e98074870acc92e065d7a6c7
Subilan <suyuanliang1995@gmail.com>
2021-08-30 15:18:44 +0800

    fix

--- src/interface.ts
+++ src/interface.ts
@@ -37,6 +37,7 @@ interface ServerInformation {
 interface InstanceInformation {
     bandwidth: number,
     datadisk: InstanceDisk,
+    systemdisk: InstanceDisk,
     type: string,
     zone: string
 }
