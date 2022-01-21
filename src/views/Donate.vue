742b28cb93d4aea69d59c58988aec038b045735b
Subilan <suyuanliang1995@gmail.com>
2022-01-22 02:08:52 +0800

    update data

--- src/views/Donate.vue
+++ src/views/Donate.vue
@@ -14,27 +14,27 @@
 					SEATiDE 的资金使用原理
 				</h1>
 				<p v-view.once="flowUp">
-					SEATiDE 的开销主要来自两个方面：Minecraft
-					服务器和网页服务器。其中网页服务器使用的是阿里云轻量应用服务器（香港），每个月固定缴纳
-					<span class="money">￥34</span>。Minecraft
+					SEATiDE 的开销主要来自实例运行（计算类）和服务器备份和数据存储（存储类）。SEATiDE
 					服务器运行在阿里云 ECS
 					的<strong>抢占式实例</strong>上，根据阿里云官方的描述，其根据市场供需结构和库存来决定扣款时机和扣款数额。通常情况下扣款数额不会过于偏离<strong>第一个小时的价格</strong>。当前我们使用的服务器类型是
 					<strong>ecs.g6.xlarge</strong>，第一个小时的价格为
-					<span class="money">￥0.205</span>。
+					<span class="money">￥0.159</span>。
 				</p>
 				<p v-view.once="flowUp">
-					我们通过统计 2021 年 2 月、3 月初以及 6
-					月三个时间段运营的账单情况，得出了如下结论
+					我们通过统计 2021 年 2 月至 2022 年 1 月的账单，得出了如下结论。括号后注明的是与上次统计相比的变化。
 				</p>
 				<ul v-view.once="flowUp">
 					<li>
-						平均扣款量（扣款时间不均匀）：<span class="money"
-							>￥0.19</span
+						计算类平均扣款量（时间不均匀）：<span class="money"
+							>￥0.17（-0.02）</span
 						>
 					</li>
-					<li>最高收费：<span class="money">￥0.5</span></li>
-					<li>最低收费：<span class="money">￥0.01</span></li>
-					<li>收费数额众数：<span class="money">￥0.17</span></li>
+					<li>计算类最高收费：<span class="money">￥1.29（+0.79）</span></li>
+					<li>计算类最低收费：<span class="money">￥0.01</span></li>
+					<li>计算类收费数额众数：<span class="money">￥0.18（+0.01）</span></li>
+					<li>存储类平均扣款量（时间不均匀）<span class="money">￥0.06</span></li>
+					<li>计算类月均收费次数：243</li>
+					<li>存储类月均收费次数：201</li>
 				</ul>
 				<p v-view.once="flowUp">
 					我们会不定期向阿里云账户内充入余额以支付 Minecraft
@@ -46,18 +46,17 @@
 				<h1 v-view.once="flowUp" class="primary-text">如何捐助</h1>
 				<p v-view.once="flowUp">
 					免责声明：<strong
-						>未满 18 岁的未成年人请在家长同意后进行捐助，SEATiDE
-						不接受未成年人未经允许的捐助。</strong
+						>未满 18 岁的未成年人请在家长同意后进行捐助，SEATiDE 从未教唆未成年人「为游戏充值」。捐助即代表你已经阅读并知晓该声明。</strong
 					>
 				</p>
 				<p v-view.once="flowUp">
 					此处提供支付宝和微信支付的二维码，如需其它支付方式（不支持 QQ 支付）可联系服主。若要捐助，扫描二维码即可，付款时<strong
 						>请备注你的 Minecraft 用户名</strong
 					>，以便我们写入鸣谢名单（低于
-					<span class="money">￥0.5</span>
-					的款项恕不写入）。无偿捐助支持
+					<span class="money">￥5</span>
+					的款项恕不写入）。支持
 					<strong>5 天无理由全额退款</strong>，超过 5
-					天若仍想退款可联系服主。
+					天若仍因某些原因仍需退款可联系服主。
 				</p>
 				<div v-view.once="flowUp" class="pay">
 					<img
@@ -74,7 +73,7 @@
 				<h1 v-view.once="flowUp" class="primary-text">感谢捐助</h1>
 				<p v-view.once="flowUp">
 					感谢这些人的无偿捐助，帮助了 SEATiDE
-					发展和维持运行。排名使用洗牌算法随机排列，刷新可重排。
+					发展和维持运行。排名使用洗牌算法随机排列，刷新页面可重排。
 				</p>
 				<ul v-view.once="flowUp">
 					<li v-for="(x, i) in shuffle(donators)" :key="i">
