7c84fb847b78bdd401b29ebbfbb72121f7e89cba
Subilan <suyuanliang1995@gmail.com>
2022-04-05 17:49:12 +0800

    add links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,6 +16,10 @@
 					注：如无特殊说明，所有文字的著作权和游戏摄影作品使用权归 SEATiDE 所有。侵权必究。
 				</p>
 			</div>
+			<div class="links">
+				<a href="https://sotap.org" target="_blank">SoTap</a> ·
+				<a href="https://i.teriri.cc/" target="_blank">桜酱的温馨小窝</a> · <a href="https://subilan.win" target="_blank">服主博客</a>
+			</div>
 			<div class="button-list">
 				<icon-btn
 					@click.native="$open('https://github.com/seatidemc')"
@@ -46,6 +50,28 @@ footer {
 	box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.4);
 	background: #212121;
 	color: white;
+	.links {
+		@media (max-width: 1000px) {
+			position: static;
+			text-align: center;
+			margin: 8px 0;
+		}
+		position: absolute;
+		right: 0;
+		bottom: calc(32px + 14px);
+		text-align: right;
+		color: @textmidwhite;
+		font-size: 14px;
+		a {
+			border-bottom: 1px solid rgba(255,255,255,.3);
+			transition: all .2s ease;
+			display: inline-block;
+			&:hover {
+				color: white;
+				border-color: white;
+			}
+		}
+	}
 
 	.footer-container {
 		display: block;
@@ -77,7 +103,10 @@ footer {
 .button-list {
 	display: flex;
 	> * {
-		margin: 0 5px;
+		margin: 8px 5px;
+	}
+	@media (max-width: 1000px) {
+		justify-content: center;
 	}
 	@media screen and (min-width: 1000px) {
 		position: absolute;
