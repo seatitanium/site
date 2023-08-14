f42dcb1cfc099f9e3823dba63a057580052ed996
Subilan <suyuanliang1995@gmail.com>
2023-08-14 15:10:18 +0800

    +qq

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -199,8 +199,8 @@
               <p v-view.once="flowUp"><strong>单击图标可加入相应的交流群，在那里你可以获取最新、最准确的消息。</strong></p>
             </div>
             <div class="welcome-btns">
-              <btn class="primary-a shadow arrow" size="large" icon="arrow-top-right">立即加入 Seati 讨论群</btn>
-              <btn class="primary-o shadow arrow" size="large" icon="arrow-top-right">立即加入 Oasis 讨论群</btn>
+              <btn class="primary-a shadow arrow" href="https://qm.qq.com/cgi-bin/qm/qr?k=tcoJ_CF6AjAWGQS62TxGRIidea_4tqw7&jump_from=webapi" size="large" icon="arrow-right">立即加入 Seati 讨论群</btn>
+              <btn class="primary-o shadow arrow" href="https://qm.qq.com/cgi-bin/qm/qr?k=SCJedgwusNyNbwfz-ySVBu8am63VFSHF&jump_from=webapi" size="large" icon="arrow-right">立即加入 Oasis 讨论群</btn>
             </div>
           </div>
         </div>
